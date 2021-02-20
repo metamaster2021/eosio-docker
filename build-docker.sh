fd43980658ca7c5f4ba5aa60850e47d0a9034d95
masteraychen <76104526+masteraychen@users.noreply.github.com>
2021-02-20 23:52:20 +0800

    Update build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/bash
 
-ver=$1
+mgpchain=$1
+ver=$2
 [[ -z "$ver" ]] && ver='v2.0.9'
 
 img="mgp/eos:${ver}"
 
 cd $ver && docker build -t $img .
+docker login --username tigerich --password $mgpchain 
+docker push $img
