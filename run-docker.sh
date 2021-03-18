4160d636f6f5ed899290bd7a37bbfa0ce5001c41
Dragon Master <tigerich@users.noreply.github.com>
2021-03-18 11:29:41 +0800

    Create run-docker.sh

---
+++ run-docker.sh
@@ -0,0 +1,19 @@
+param=$1
+node=mgp-testnet #modify this to meet your needs
+
+EOSIMG="mgp/eos:v2.0.9"
+MGP=/opt/mgp
+BIN_D=$MGP/bin
+HOME_D=$MGP
+CONF_D=$HOME_D/conf
+DATA_D=$HOME_D/data
+LOGS_D=$HOME_D/logs
+mkdir -p $LOGS_D $DATA_D $CONF_D $BIN_D
+
+docker run --name $node --network=mgpnetwork -itd --rm -p 9806:9806 -p 8888:8888 \
+  -v $HOME_D:/opt/mgp -w /opt/mgp \
+  -v $BIN_D:/opt/mgp/bin \
+  -v $DATA_D:/opt/mgp/data \
+  -v $CONF_D:/opt/mgp/conf \
+  -v $LOGS_D:/opt/mgp/logs \
+  $EOSIMG /opt/mgp/bin/start.sh $node $param
