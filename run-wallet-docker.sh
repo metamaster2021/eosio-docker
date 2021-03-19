2eaa3bf26dd1b7af0febd581079ae6fc86d2433a
tigerich <tigerich@163.com>
2021-03-19 10:29:56 +0800

    update wallet bin

--- run-wallet-docker.sh
+++ run-wallet-docker.sh
@@ -2,7 +2,7 @@ EOSIMG="mgpchain/eosio:v2.0.10"
 MGP=/opt/mgp/node_wallet
 
 docker run --name mgp-wallet --network=mgpnetwork --rm -itd -p 5555:8888 \
-   -v $MGP/bin:/opt/mgp/bin \
+   -v $MGP/bin:$MGP/bin \
    -v $MGP/wallet:/root/eosio-wallet \
    -w /root/eosio-wallet \
-   $EOSIMG bash -c "/opt/mgp/bin/wallet.sh"
+   $EOSIMG bash -c "$MGP/bin/wallet.sh"
