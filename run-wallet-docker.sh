5f2cb4261657fdad22fffa82b1bd519c4533c47b
tigerich <tigerich@163.com>
2021-03-19 12:21:13 +0800

    update

--- run-wallet-docker.sh
+++ run-wallet-docker.sh
@@ -4,6 +4,7 @@ MGP=/opt/mgp/node_wallet
 docker run --name mgp-wallet --network=mgpnetwork --rm -itd -p 5555:8890 \
    -v $MGP/bin:$MGP/bin \
    -v $MGP/conf:$MGP/conf \
+   -v $MGP/logs:$MGP/logs \
    -v $MGP/data:/root/eosio-wallet \
    -w /root/eosio-wallet \
    $EOSIMG $MGP/bin/wallet.sh
