98232544bdac83b2174ec697542d08b5c71b7c6f
tigerich <tigerich@163.com>
2021-03-19 12:06:50 +0800

    update wallet

--- node_wallet/bin/wallet.sh
+++ node_wallet/bin/wallet.sh
@@ -1,5 +1,8 @@
+MGPDIR="/opt/mgp/node_wallet/"
+CONFDIR=$MGPDIR/conf
 DATDIR="/root/eosio-wallet"
 LOGDIR=$DATDIR/logs
+
 mkdir -p $LOGDIR
 
 TIMESTAMP=$(/bin/date +%s)
@@ -7,7 +10,7 @@ NEW_LOG="mgp-$TIMESTAMP.log"
 
 #apt update && apt install -y libusb-1.0-0
 #/opt/mgp/stop_wallet.sh
-keosd --config-dir $DATDIR -d $DATDIR --unix-socket-path $DATDIR/keosd.sock >> $LOGDIR/$NEW_LOG 2>&1 &
+keosd --config-dir $CONFDIR -d $DATDIR --unix-socket-path $DATDIR/keosd.sock >> $LOGDIR/$NEW_LOG 2>&1 &
 
 echo $! > $DATDIR/wallet.pid
 unlink $LOGDIR/mgp.log
--- run-wallet-docker.sh
+++ run-wallet-docker.sh
@@ -1,8 +1,8 @@
 EOSIMG="mgpchain/eosio:v2.0.10"
 MGP=/opt/mgp/node_wallet
 
-docker run --name mgp-wallet --network=mgpnetwork --rm -itd -p 5555:8888 \
-   -v $MGP/bin:$MGP/bin \
-   -v $MGP/wallet:/root/eosio-wallet \
+docker run --name mgp-wallet --network=mgpnetwork --rm -itd -p 5555:8890 \
+   -v $MGP:$MGP \
+   -v $MGP/data:/root/eosio-wallet \
    -w /root/eosio-wallet \
    $EOSIMG bash -c "$MGP/bin/wallet.sh"
